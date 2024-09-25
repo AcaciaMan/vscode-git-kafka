61d0db53c97502fd0e10fca8e7c5ccff374d5cf7
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2024-09-25 17:26:23 +0300

    Started calculate of biggest dirs

--- vscode/vscode-git-kafka/src/m_grep/providerGitGrep.ts
+++ vscode/vscode-git-kafka/src/m_grep/providerGitGrep.ts
@@ -3,6 +3,8 @@
 
 import * as vscode from "vscode";
 import { exec } from "child_process";
+const fs = require("fs");
+import * as path from "path";
 
 export class ProviderGitGrep implements vscode.WebviewViewProvider {
   public static readonly viewType = 'myExtension.myWebview';
@@ -28,6 +30,9 @@ export class ProviderGitGrep implements vscode.WebviewViewProvider {
             switch (data.type) {
                 case 'search':
                     await this._search(data.searchTerm);
+                    break;
+                case 'biggestDirs':
+                    await this._biggestDirs();
                     break;
             }
         });
@@ -125,6 +130,117 @@ export class ProviderGitGrep implements vscode.WebviewViewProvider {
         return new ProviderGitGrep(context);
     }
 
+
+    private async _biggestDirs() {
+                vscode.window.showInformationMessage("Biggest Dirs...");
+                vscode.window.showInformationMessage("message1");
+                vscode.window.showInformationMessage("message2");
+                vscode.window.showInformationMessage("message3");
+                vscode.window.showInformationMessage("message4");
+                vscode.window.showInformationMessage("message5");
+
+    }
+
+    private async _biggestDirs1() {
+
+
+        const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+        if (!workspaceFolder) {
+            vscode.window.showErrorMessage("No workspace folder found");
+            return;
+        }
+
+
+
+
+
+                             /* vscode.window.showInformationMessage(
+                          `Dir:` +
+                            fs
+                              .statSync(workspaceFolder.uri.fsPath)
+                              .isDirectory()
+                        );
+                        vscode.window.showInformationMessage(
+                          `Dir:` +
+                            fs.statSync(workspaceFolder.uri.fsPath).isFile()
+                        );
+                        */
+
+        // do in steps
+        // get first level files list with git grep -l --max-depth=1 ""
+        // for files in list, get file size with fs.stat
+        // for dirs in list, repeat with git grep -l --max_depth=1 "" in cwd/dir
+
+        const command = `git grep -l --max-depth=1 ""`;
+
+        let sDirs = "";
+        let files: string[] = [];
+        exec(
+            command,
+            { cwd: workspaceFolder.uri.fsPath, timeout: 10000 },
+            (error, stdout, stderr) => {
+              if (error) {
+                vscode.window.showErrorMessage(`Error: ${error.message}`);
+                vscode.window.showErrorMessage(`Error: ${stderr}`);
+                return;
+              }
+
+              files = stdout.split("\n");
+               vscode.window.showInformationMessage(stdout);
+
+              const list = Array.from(
+                { length: files.length },
+                (_, index) => index
+              );
+              vscode.window.showInformationMessage(`List: ${list.length}`);
+              vscode.window.showInformationMessage(`List: ${list}`);
+
+             
+
+              //await Promise.all(
+              //  list.map((_, index) => {
+              //    return fs.promises.stat(filePath + `_${index}.md`);
+              //  })
+              // );
+
+              files.pop(); // remove last empty line
+
+              files.forEach((file) => {
+                const filePath = path.join(workspaceFolder.uri.fsPath, file);
+                vscode.window.showInformationMessage(`File:` + filePath);
+              });
+            }
+        );
+
+                files.forEach((file) => {
+                  const filePath = path.join(workspaceFolder.uri.fsPath, file);
+                  vscode.window.showInformationMessage(`File:` + filePath);
+
+                  try {
+                    const stats = fs.statSync(filePath);
+                    vscode.window.showInformationMessage("Stats: " + stats);
+                    if (stats.isDirectory()) {
+                      sDirs += `${filePath} is a directory\n`;
+                    } else {
+                      sDirs += `${filePath} is a file\n`;
+                    }
+                  } catch (err) {
+                    vscode.window.showErrorMessage(
+                      `Error: ${(err as Error).message}`
+                    );
+                  }
+                });
+
+
+
+        const outputChannel =
+            vscode.window.createOutputChannel("Biggest Dirs");
+        outputChannel.append(sDirs);
+        outputChannel.show();
+
+
+
+    }
 }
 
 
