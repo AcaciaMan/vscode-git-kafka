a812417259f2eb9a523fbc96d2fc14c978661f05
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2024-10-04 01:49:34 +0300

    Get workspace folder from vscode

--- vscode/vscode-git-kafka/src/m_tasks/m_task.ts
+++ vscode/vscode-git-kafka/src/m_tasks/m_task.ts
@@ -6,22 +6,27 @@ import { M_State } from './m_state';
 export class M_Task {
   created_at: Date;
   m_global: M_Global = M_Global.getInstance();
-  workspaceFolder: vscode.WorkspaceFolder = this.m_global.workspaceFolder ?? (() => { throw new Error("WorkspaceFolder is undefined"); })();
+  workspaceFolder: vscode.WorkspaceFolder =
+    vscode.workspace.workspaceFolders?.[0] ??
+    (() => {
+      throw new Error("WorkspaceFolder is undefined");
+    })();
   aDirs: M_Dir[] = [];
   mState: M_State = new M_State();
 
-    constructor() {
-        this.created_at = new Date();
-    }
+  constructor() {
+    this.created_at = new Date();
+  }
 
+  getId(): string {
+    return this.created_at.getTime().toString();
+  }
 
-    getId(): string {
-        return this.created_at.getTime().toString();
-    }
-
-    async init() {
-        const mCalcDirs = await this.m_global.getCalcDirs();
-        const dDirs = mCalcDirs.dirs;
-        this.aDirs = Object.values(dDirs).map((dir) => new M_Dir(dir.parent, dir.dir, 0, 0));
-    }
+  async init() {
+    const mCalcDirs = await this.m_global.getCalcDirs();
+    const dDirs = mCalcDirs.dirs;
+    this.aDirs = Object.values(dDirs).map(
+      (dir) => new M_Dir(dir.parent, dir.dir, 0, 0)
+    );
+  }
 }
