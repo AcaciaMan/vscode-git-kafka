4793aba069a919155694adb876eb36fd66287ecd
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2024-10-04 23:41:44 +0300

    Try to run solr

--- vscode/vscode-git-kafka/src/test/some.test.ts
+++ vscode/vscode-git-kafka/src/test/some.test.ts
@@ -1,4 +1,5 @@
 import * as assert from "assert";
+const solr = require("solr-client");
 
 // test suite to test some typescript functionality
 describe("Some Test Suite", () => {
@@ -26,5 +27,33 @@ describe("Some Test Suite", () => {
 
 
     });
+
+    it("Sample test 4", (done) => {
+        const client = solr.createClient({
+            host: "localhost",
+            port: "8983",
+            core: "events",
+            path: "/solr",
+        });
+
+        console.log("client: ", client);
+
+
+        const query = client.query().q("*");
+        console.log("Executing query: ", query);
+        
+        client.search(query, function (err: any, obj: any) {
+            console.log("searching for all docs");
+            if (err) {
+                console.log(err);
+                done(err);
+            } else {
+                console.log("Obj", obj);
+                done();
+            }
+        });
+
+
 });
-//
+});
+
